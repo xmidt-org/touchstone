135e54dd191217b0398d93a5cc6f75bfa614b145
johnabass <johnbass1017@yahoo.com>
2022-08-23 09:36:45 -0700

    forgot to add this test file

---
+++ touchhttp/instrumenter_test.go
@@ -0,0 +1,72 @@
+package touchhttp
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/xmidt-org/touchstone"
+	"go.uber.org/fx"
+	"go.uber.org/fx/fxtest"
+)
+
+type NewServerInstrumenterSuite struct {
+	suite.Suite
+}
+
+func (suite *NewServerInstrumenterSuite) TestDefaults() {
+	var (
+		si ServerInstrumenter
+
+		app = fxtest.New(
+			suite.T(),
+			touchstone.Provide(),
+			fx.Provide(
+				NewServerInstrumenter(),
+			),
+			fx.Populate(&si),
+		)
+	)
+
+	app.RequireStart()
+	app.RequireStop()
+}
+
+func (suite *NewServerInstrumenterSuite) TestNamed() {
+	var (
+		app = fxtest.New(
+			suite.T(),
+			touchstone.Provide(),
+			fx.Provide(
+				fx.Annotated{
+					Name: "servers.main",
+					Target: NewServerInstrumenter(
+						ServerLabel, "servers.main",
+					),
+				},
+				fx.Annotated{
+					Name: "servers.health",
+					Target: NewServerInstrumenter(
+						ServerLabel, "servers.health",
+					),
+				},
+			),
+			fx.Invoke(
+				fx.Annotate(
+					func(ServerInstrumenter) {},
+					fx.ParamTags(`name:"servers.main"`),
+				),
+				fx.Annotate(
+					func(ServerInstrumenter) {},
+					fx.ParamTags(`name:"servers.health"`),
+				),
+			),
+		)
+	)
+
+	app.RequireStart()
+	app.RequireStop()
+}
+
+func TestNewServerInstrumenter(t *testing.T) {
+	suite.Run(t, new(NewServerInstrumenterSuite))
+}
